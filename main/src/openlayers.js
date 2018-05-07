ef1c195de59b77bfd121d6d9d085ecc6ac857af1
Roman Zoller <roman.zoller@camptocamp.com>
2018-05-07 17:49:49 +0200

    Override renderFrame_ to measure render time

--- main/src/openlayers.js
+++ main/src/openlayers.js
@@ -1,10 +1,26 @@
+import CanvasMap from 'ol/canvasmap';
 import View from 'ol/view';
 import proj from 'ol/proj';
 import { apply } from 'ol-mapbox-style';
 import { startPerformanceRecording, stopPerformanceRecording, stopPRAfterATime } from './sharedperformance/performance';
 
 
-const map = apply('map', './styles/mapbox-roads-basic.json');
+class InstrumentedCanvasMap extends CanvasMap {
+  renderFrame_(time) {
+    const beforeRender = performance.now();
+    super.renderFrame_(time);
+    const afterRender = performance.now();
+    const renderTime = afterRender - beforeRender;
+    console.log('rendered in', renderTime);
+  }
+}
+
+const map = new InstrumentedCanvasMap({
+  target: 'map',
+});
+
+apply(map, './styles/mapbox-roads-basic.json');
+
 map.setView(new View({
   center: proj.fromLonLat([6, 46]),
   zoom: 8,
