85f60c834de865ad5258147370b3d2bc497fbc08
Kalaboum <nataniel.hofer@gmail.com>
2018-05-24 15:18:35 +0200

    analyzing the perfs of the benchtests

---
+++ main/src/filesIO/metaPerfWriter.js
@@ -0,0 +1,25 @@
+import { metaperf, metaPerfLogsFileName } from '../sharedperformance/metaPerf';
+import { outputJSON } from './utils';
+
+const fs = require('fs');
+
+function writeMetaPerf(dirName) {
+  if (!fs.existsSync(dirName)) {
+    throw new Error(`no such dir : ${dirName}`);
+  } else {
+    const perfs = metaperf(dirName);
+    outputJSON(perfs, metaPerfLogsFileName, dirName);
+  }
+}
+
+function main() {
+  const args = process.argv.splice(2);
+  args.forEach((dirName) => {
+    writeMetaPerf(dirName);
+  });
+}
+
+if (typeof require !== 'undefined' && require.main === module) {
+  main();
+}
+
